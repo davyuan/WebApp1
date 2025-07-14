bc4a89b8742edd5a914f7d926f9f917ca42dcae8
David Yuan <davyuan@live.com>
2025-07-14 17:35:39 -0500

    switch to github from ado

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Pages
 {
@@ -12,9 +13,22 @@ namespace WebApplication1.Pages
             _logger = logger;
         }
 
+        [BindProperty]
+        [Required(ErrorMessage = "Zip code is required.")] // RequiredAttribute is defined in System.ComponentModel.DataAnnotations
+        [RegularExpression(@"^\d{5}$", ErrorMessage = "Zip code must be a 5 digit number.")]
+        public string ZipCode { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public void OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                // Handle validation errors (optional)
+            }
         }
     }
 }
